a304ce4251142a9978e62deed075afd2dae10917
zcl <hughzcl@outlook.com>
2023-10-20 17:34:08 +0800

    add readme.md

---
+++ src/vicon_ros2_bridge/README.md
@@ -0,0 +1,61 @@
+# Vicon_ROS2_Bridge
+
+## 1. Overview
+Here are the ROS2 package to publish vicon server's track data(subject, segment and marker) realtime on Ubuntu. Node `vicon_ros2_bridge` depends on ViconDataStreamSDK_Linux64 (https://www.vicon.com/software/datastream-sdk/).
+
+## 2. Packages
+**vicon_msgs**: `Marker.msg`, `Segment.msg`,`Subject.msg`,`ViconCaptureTree.msg`
+
+**Vicon_ros2_bridge**: topic name is `"vicon_capture_tree"`
+
+## 3. How to run 
+First, configure the computer IP to `192.168.10.X` network segment.
+
+run
+* `colcon build`
+* `. install/setup.bash`
+* `ros2 run vicon_ros2_bridge vicon_ros2_brige` in dir **Vicon_ROS2_Bridge**
+
+If return *"Vicon Server connect successfully!"* and show information of capture tree, means node open normal.
+
+## 4. Dependencies
+* ROS2 foxy on Ubuntu20.04
+* vicon_msgs
+
+## 5. Detials
+```
+hugh@destop:~/Vicon_ROS2_Bridge$ ros2 run vicon_ros2_bridge vicon_ros2_brige
+
+Vicon Server connect successfully!
+Segment lightweight data enable successfully!
+********************Subjects and Markers information*********************
+_______subject_______
+the number of Subject is 2
+NO: 0 Subject name is :object1
+    _______segment_______
+    the number of object1 Segment is :1
+    NO: 0 Segment name is :object1
+    _______marker_______
+    the number of object1 marker is :3
+    NO: 0 marker name is :object11
+    NO: 1 marker name is :object12
+    NO: 2 marker name is :object13
+
+NO: 1 Subject name is :object2
+    _______segment_______
+    the number of object2 Segment is :1
+    NO: 0 Segment name is :object2
+    _______marker_______
+    the number of object2 marker is :3
+    NO: 0 marker name is :object21
+    NO: 1 marker name is :object22
+    NO: 2 marker name is :object23
+
+***********************************************************************
+```
+
+
+  
+
+
+
---
+++ src/vicon_ros2_bridge/src/ViconSDK.cpp
@@ -0,0 +1,154 @@
+#include "ViconSDK.h"
+
+ViconSDK::ViconSDK()
+{
+
+	ViconDataStreamSDK::CPP::Output_Connect output_connect = myClient.Connect("192.168.10.1");
+	switch (output_connect.Result)
+	{
+	case 2:
+		cout << "Vicon Server connect successfully!" << endl;
+		break;
+	case 3:
+		cout << "The HostName parameter passed to Connect() is invalid" << endl;
+		break;
+	case 5:
+		cout << "Vicon SDK was called whilst already connected to a DataStream" << endl;
+		break;
+	case 6:
+		cout << "Vicon Client could not establish a connection to the DataStream server" << endl;
+		break;
+	default:
+		break;
+	}
+
+
+	ViconDataStreamSDK::CPP::Output_EnableSegmentData output_enableSegmentData = myClient.EnableSegmentData();
+	switch (output_enableSegmentData.Result)
+	{
+	case 2:
+		cout << "Segment lightweight data enable successfully!" << endl;
+		break;
+	case 9:
+		cout << "Segment lightweight data enable unsuccessfully, because do not have a connection to the DataStream server!" << endl;
+		break;
+	default:
+		break;
+	}
+
+	myClient.SetStreamMode(ViconDataStreamSDK::CPP::StreamMode::ClientPull);
+	myClient.EnableMarkerData();
+
+	sleep(1);
+    ViconSubAndSegShow();
+}
+
+void ViconSDK::ViconSubAndSegShow()
+{
+	myClient.GetFrame();
+	OutputSubCount_ = myClient.GetSubjectCount();
+	cout << "********************Subjects and Markers information*********************" << endl;
+    cout <<"_______subject_______\n";
+    cout << "the number of Subject is " << OutputSubCount_.SubjectCount << endl;
+
+//TODO:GetSubjectName
+	for (unsigned int SubCount = 0; SubCount < OutputSubCount_.SubjectCount; SubCount++)
+	{
+		auto subName = myClient.GetSubjectName(SubCount);
+		cout << "NO: " << SubCount << " Subject name is :" << subName.SubjectName << endl;
+
+		cout << "    _______segment_______\n";
+		OutputSegCount_ = myClient.GetSegmentCount(subName.SubjectName);
+		cout << "    the number of " << subName.SubjectName << " Segment is :" << OutputSegCount_.SegmentCount << endl;
+		for (unsigned int SegCount = 0; SegCount < OutputSegCount_.SegmentCount; SegCount++)
+		{
+			auto segName = myClient.GetSegmentName(subName.SubjectName, SegCount);
+			//cout << SegCount << " Segment name is :" << OutputSegName.SegmentName << endl;
+			cout << "    NO: " << SegCount << " Segment name is :" << segName.SegmentName << endl;
+		}
+		cout << "    _______marker_______\n";
+		OutputMarkCount_ = myClient.GetMarkerCount(subName.SubjectName);
+		cout << "    the number of " << subName.SubjectName << " marker is :" << OutputMarkCount_.MarkerCount << endl;
+		for (unsigned int MarkerCount = 0; MarkerCount < OutputMarkCount_.MarkerCount; MarkerCount++)
+		{
+			auto markName = myClient.GetMarkerName(subName.SubjectName, MarkerCount);
+			//cout << SegCount << " Segment name is :" << OutputSegName.SegmentName << endl;
+			cout << "    NO: " << MarkerCount << " marker name is :" << markName.MarkerName << endl;
+		}
+		//cout << "-----------------------------------";
+		cout << endl;
+	}
+	cout << "***********************************************************************\n" << endl;
+}
+
+Eigen::Matrix<double, 6,1> ViconSDK::getSegHomoTransMatrix(std::string subjectName,std::string segmentName){
+    Eigen::Matrix<double,6,1> homoTransMatrix;
+    Output_GetSegmentGlobalTranslation globalTrans = myClient.GetSegmentGlobalTranslation(subjectName, segmentName);
+    Output_GetSegmentGlobalRotationEulerXYZ globalRotEular = myClient.GetSegmentGlobalRotationEulerXYZ(subjectName, segmentName);
+    Eigen::Map<Eigen::Vector3d> rot(globalRotEular.Rotation);
+    Eigen::Map<Eigen::Vector3d> pos(globalTrans.Translation);
+    homoTransMatrix.block<3,1>(0,0) = rot;
+    homoTransMatrix.block<3,1>(3,0) = pos;
+
+    return homoTransMatrix;
+}
+
+
+captureTree::marker ViconSDK::getOneMarkerTrans(uint subjectIndex, uint markerCount){
+	captureTree::marker marker;
+    std::string subjectName = myClient.GetSubjectName(subjectIndex).SubjectName;
+    marker.name = myClient.GetMarkerName(subjectName, markerCount).MarkerName;
+    marker.index = markerCount;
+    marker.x = myClient.GetMarkerGlobalTranslation(subjectName, marker.name).Translation[0];
+    marker.y = myClient.GetMarkerGlobalTranslation(subjectName, marker.name).Translation[1];
+    marker.z = myClient.GetMarkerGlobalTranslation(subjectName, marker.name).Translation[2];
+    return marker;
+}
+
+captureTree::segment ViconSDK::getSegmentTrans(uint subjectIndex, uint segmentCount){
+    captureTree::segment segment;
+    std::string subjectName = myClient.GetSubjectName(subjectIndex).SubjectName;
+    segment.name = myClient.GetSegmentName(subjectName, segmentCount).SegmentName;
+    segment.index = segmentCount;
+    segment.globalTrans = getSegHomoTransMatrix(subjectName, segment.name);
+    return segment;
+}
+
+captureTree::subject ViconSDK::getSubjectTrans(uint subjectIndex){
+    captureTree::subject subject;
+    subject.name = myClient.GetSubjectName(subjectIndex).SubjectName;
+    uint segmentsNum = myClient.GetSegmentCount(subject.name).SegmentCount;
+    for(int i = 0; i < segmentsNum; i++){
+        auto segmentTrans = getSegmentTrans(subjectIndex, i);
+        subject.segments.push_back(segmentTrans);
+    }
+    uint markersNum = myClient.GetMarkerCount(subject.name).MarkerCount;
+    for(int i = 0; i < markersNum; i++){
+        auto markerTrans = getOneMarkerTrans(subjectIndex, i);
+        subject.markers.push_back(markerTrans);
+    }
+    return subject;
+}
+
+captureTree::captureTree ViconSDK::getCaptureTree(){
+    captureTree::captureTree captureTree;
+    uint subjectNum = myClient.GetSubjectCount().SubjectCount;
+    captureTree.frameNumber = myClient.GetFrameNumber().FrameNumber;
+    captureTree.latency = myClient.GetLatencyTotal().Total;
+    captureTree.frameRate = myClient.GetFrameRate().FrameRateHz;
+    for(int i = 0; i < subjectNum; i++){
+        auto subjectTrans = getSubjectTrans(i);
+        captureTree.subjects.push_back(subjectTrans);
+    }
+
+    return captureTree;
+}
+
+
+
+
+
+
+
+
+
